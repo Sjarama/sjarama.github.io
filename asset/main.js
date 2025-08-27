1e73c1d322bbe0a9461157b5f4da900dc408a7c1
PV-Alumno <PV-Alumno@duoc.acad>
2025-08-27 10:15:14 -0400

    feat: main js que ajusta el html con un mensaje y timestamp

---
+++ asset/main.js
@@ -0,0 +1,7 @@
+console.log("mundo")
+let message="soy pro en fullstack"
+document.writeln("<h3>"+message+ "</h3");
+
+const timestamp = Date.now();
+let mensaje=" hoy en timesmap es:"+ timestamp;
+document.writeln("<p>"+mensaje +"</p>")
