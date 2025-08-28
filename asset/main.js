e3ed01c544c6f489301053d413f335b4c4cebb2b
PV-Alumno <PV-Alumno@duoc.acad>
2025-08-28 11:08:06 -0400

    feat: validar edad con js y html

--- asset/main.js
+++ asset/main.js
@@ -1,3 +1,27 @@
+const formulario = document.getElementById('formulario');
+const edad_persona = document.getElementById('edad');
+const resultado = document.getElementById('resultado');
+msg= "<p> Tu edad fue verificada tu eres: </p>"
+console.log (edad_persona);
+
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault(); 
+    const edad = parseInt(edad_persona.value);
+
+    if (isNaN(edad)) {
+        resultado.innerHTML=msg + 'Por favor, ingresa una edad válida.';
+    } else if (edad < 18 && edad >0) {
+        
+         resultado.innerHTML=msg + 'Eres menor de edad.';
+    } else if(edad>=18) {
+       resultado.innerHTML=msg +'Eres mayor de edad.';
+    }
+    else{
+        resultado.innerHTML=msg + 'Por favor, ingresa una edad válida.';
+    }
+});
+
+
 console.log("mundo")
 let message="soy pro en fullstack"
 document.writeln("<h3>"+message+ "</h3");
